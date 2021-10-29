c64e2a79600f071f665932606eaafa42b8eb2a79
Andres Rodriguez <kflex25@MacBook-Pro-de-Andres.local>
2021-10-29 10:57:09 -0600

    Strings

---
+++ src/main/java/com/codingdojo/StringsApplication.java
@@ -0,0 +1,29 @@
+package com.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String Strings() {
+		return "Hello client! How are you doing";
+		
+	}
+	@RequestMapping(value="/random",method=RequestMethod.GET)
+	public String random() {
+		return "Spring Boot is great! So easy to just respond with strings";
+		
+	}
+	
+
+}
